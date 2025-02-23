f8bdda580ec53c7bead6f5fc97dc1ecbfbc70a03
JohnMarcBautista <johnmarcbautista1234r@gmail.com>
2025-02-23 13:28:47 -0800

    the start of a new era

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import Carousel from "@/components/Carousel";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen bg-gradient-to-br from-black via-gray-900 to-black bg-[url('/stars-bg.jpg')] bg-cover bg-center flex flex-col items-center justify-center p-8 text-white">
-      {/* Black overlay for bottom 1/3 of the page */}
+    <div className="relative min-h-screen flex flex-col items-center justify-center p-8 text-white bg-[url('/stars-bg.jpg')] bg-cover bg-center">
+      {/* Galaxy overlay for the top 2/3 of the page */}
+      <div className="absolute top-0 left-0 w-full h-2/3 bg-[url('/backgrounds/sunny.jpg')] bg-cover bg-center pointer-events-none"></div>
+
+
+      {/* Black overlay for the bottom 1/3 of the page */}
       <div className="absolute bottom-0 left-0 w-full h-1/3 bg-black pointer-events-none"></div>
-      
+
       <div className="relative z-10 flex flex-col items-center">
         <h1 className="text-3xl md:text-5xl font-extrabold mb-4 drop-shadow-2xl">
           Locked In
